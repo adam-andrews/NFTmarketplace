002e61bc86fc09a8f8106497922136920656fe74
Adam Andrews <adam@Adams-MacBook-Air.local>
2021-11-11 14:55:23 +0000

    Added Components Folder

--- pages/apps.js
+++ pages/apps.js
@@ -1,12 +1,12 @@
-export default function Course() {
+export default function apps() {
 
-    const apps = [
-      "Flappy bird",
-      "Subway surfers",
-      "Clash of clans",
-      "Minecraft",
-      "Instagram",
-      "Snapchat",
+    const lectures = [
+      "How to init App",
+      "How to get a help",
+      "Introduction to Solidity",
+      "Programing in C++",
+      "How to write For Loops",
+      "Safe operator",
     ]
   
     return (
